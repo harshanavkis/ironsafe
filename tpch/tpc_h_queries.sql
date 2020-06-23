933a376f9d908d78ba92a0ffaa27289e5602091a
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2020-06-23 14:28:02 +0100

    FIX: TPC-H queries
    This commit fixes 4 TPC-H queries, leaves 4 unchanged.
    
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- tpch/tpc_h_queries.sql
+++ tpch/tpc_h_queries.sql
@@ -1,4 +1,5 @@
 /* TPC-H Query 1 */
+/* Tested and works */
 select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice * (1 - l_discount)) as sum_disc_price, sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from LINEITEM where l_shipdate <= '1998-08-15' group by l_returnflag, l_linestatus order by l_returnflag, l_linestatus;
 
 /*Host query: Q1*/
@@ -74,31 +75,36 @@ select * from LINEITEM where l_shipdate >= '1994-01-01' and l_shipdate < '1995-0
 select strftime('%Y', l_shipdate)
 
 /* TPC-H Query 7 */
-/* FIXME:( */
-select supp_nation, cust_nation, l_year, sum(volume) as revenue from ( select n1.n_name as supp_nation, n2.n_name as cust_nation, (select strftime('%Y', l_shipdate) as l_year), l_extendedprice * (1 - l_discount) as volume from SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2 where s_suppkey = l_suppkey and o_orderkey = l_orderkey and c_custkey = o_custkey and s_nationkey = n1.n_nationkey and c_nationkey = n2.n_nationkey and ((n1.n_name = 'JAPAN' and n2.n_name = 'INDIA') or (n1.n_name = 'INDIA' and n2.n_name = 'JAPAN')) and l_shipdate between '1995-01-01' and '1996-12-31') as shipping group by supp_nation, cust_nation, l_year order by supp_nation, cust_nation, l_year;
+/* Tested and works */
+select supp_nation, cust_nation, l_year, sum(volume) as revenue from ( select n1.n_name as supp_nation, n2.n_name as cust_nation, strftime('%Y', l_shipdate) as l_year, l_extendedprice * (1 - l_discount) as volume from SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2 where s_suppkey = l_suppkey and o_orderkey = l_orderkey and c_custkey = o_custkey and s_nationkey = n1.n_nationkey and c_nationkey = n2.n_nationkey and ((n1.n_name = 'JAPAN' and n2.n_name = 'INDIA') or (n1.n_name = 'INDIA' and n2.n_name = 'JAPAN')) and l_shipdate between '1995-01-01' and '1996-12-31') as shipping group by supp_nation, cust_nation, l_year order by supp_nation, cust_nation, l_year;
 
 /* Host Query: Q7 */
 select supp_nation, cust_nation, l_year, sum(volume) as revenue from TABLE1 group by supp_nation, cust_nation, l_year order by supp_nation, cust_nation, l_year;
 
 /* ssd Query: Q7 */
-select n1.n_name as supp_nation, n2.n_name as cust_nation, extract(year from l_shipdate) as l_year, l_extendedprice * (1 - l_discount) as volume from SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2 where s_suppkey = l_suppkey and o_orderkey = l_orderkey and c_custkey = o_custkey and s_nationkey = n1.n_nationkey and c_nationkey = n2.n_nationkey and ((n1.n_name = 'JAPAN' and n2.n_name = 'INDIA') or (n1.n_name = 'INDIA' and n2.n_name = 'JAPAN')) and l_shipdate between '1995-01-01' and '1996-12-31';
+select n1.n_name as supp_nation, n2.n_name as cust_nation, strftime('%Y', l_shipdate) as l_year, l_extendedprice * (1 - l_discount) as volume from SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2 where s_suppkey = l_suppkey and o_orderkey = l_orderkey and c_custkey = o_custkey and s_nationkey = n1.n_nationkey and c_nationkey = n2.n_nationkey and ((n1.n_name = 'JAPAN' and n2.n_name = 'INDIA') or (n1.n_name = 'INDIA' and n2.n_name = 'JAPAN')) and l_shipdate between '1995-01-01' and '1996-12-31';
 /*****************/
 
 /* TPC-H Query 8 */
-/* FIXME:( */
-select o_year, sum(case when nation = 'INDIA' then volume else 0 end) / sum(volume) as mkt_share from (select extract(year from o_orderdate) as o_year,	l_extendedprice * (1 - l_discount) as volume, n2.n_name as nation from PART, SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2, REGION where p_partkey = l_partkey and s_suppkey = l_suppkey and l_orderkey = o_orderkey and o_custkey = c_custkey and c_nationkey = n1.n_nationkey and n1.n_regionkey = r_regionkey and r_name = 'ASIA'	and s_nationkey = n2.n_nationkey and o_orderdate between '1995-01-01' and '1996-12-31'and p_type = 'SMALL PLATED COPPER') as all_nations group by o_year order by o_year;
+/* Tested and works */
+select o_year, sum(case when nation = 'INDIA' then volume else 0 end) / sum(volume) as mkt_share from (select strftime('%Y', o_orderdate) as o_year, l_extendedprice * (1 - l_discount) as volume, n2.n_name as nation from PART, SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2, REGION where p_partkey = l_partkey and s_suppkey = l_suppkey and l_orderkey = o_orderkey and o_custkey = c_custkey and c_nationkey = n1.n_nationkey and n1.n_regionkey = r_regionkey and r_name = 'ASIA'	and s_nationkey = n2.n_nationkey and o_orderdate between '1995-01-01' and '1996-12-31'and p_type = 'SMALL PLATED COPPER') as all_nations group by o_year order by o_year;
 
 /* Host Query: Q8 */
 select o_year, sum(case when nation = 'INDIA' then volume else 0 end) / sum(volume) as mkt_share from TABLE1 group by o_year order by o_year;
 
 /* ssd Query: Q8 */
-select extract(year from o_orderdate) as o_year,	l_extendedprice * (1 - l_discount) as volume, n2.n_name as nation from PART, SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2, REGION where p_partkey = l_partkey and s_suppkey = l_suppkey and l_orderkey = o_orderkey and o_custkey = c_custkey and c_nationkey = n1.n_nationkey and n1.n_regionkey = r_regionkey and r_name = 'ASIA'	and s_nationkey = n2.n_nationkey and o_orderdate between '1995-01-01' and '1996-12-31'and p_type = 'SMALL PLATED COPPER';
+select strftime('%Y', o_orderdate) as o_year, l_extendedprice * (1 - l_discount) as volume, n2.n_name as nation from PART, SUPPLIER, LINEITEM, ORDERS, CUSTOMER, NATION n1, NATION n2, REGION where p_partkey = l_partkey and s_suppkey = l_suppkey and l_orderkey = o_orderkey and o_custkey = c_custkey and c_nationkey = n1.n_nationkey and n1.n_regionkey = r_regionkey and r_name = 'ASIA'	and s_nationkey = n2.n_nationkey and o_orderdate between '1995-01-01' and '1996-12-31'and p_type = 'SMALL PLATED COPPER';
 /*****************/
 
 /* TPC-H Query 9 */
-/* FIXME:( */
-select nation, o_year, sum(amount) as sum_profit from (select n_name as nation, extract(year from o_orderdate) as o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount from PART, SUPPLIER, LINEITEM, PARTSUPP, ORDERS, NATION where s_suppkey = l_suppkey and ps_suppkey = l_suppkey and ps_partkey = l_partkey and p_partkey = l_partkey and o_orderkey = l_orderkey and s_nationkey = n_nationkey and p_name like '%dim%') as profit group by nation, o_year order by nation, o_year desc;
+/* Tested and works */
+select nation, o_year, sum(amount) as sum_profit from (select n_name as nation, strftime('%Y', o_orderdate) as o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount from PART, SUPPLIER, LINEITEM, PARTSUPP, ORDERS, NATION where s_suppkey = l_suppkey and ps_suppkey = l_suppkey and ps_partkey = l_partkey and p_partkey = l_partkey and o_orderkey = l_orderkey and s_nationkey = n_nationkey and p_name like '%dim%') as profit group by nation, o_year order by nation, o_year desc;
 
+/* Host Query: Q9 */
+select nation, o_year, sum(amount) as sum_profit from TABLE1 group by nation, o_year order by nation, o_year desc;
+
+/* ssd Query: Q9 */
+select n_name as nation, strftime('%Y', o_orderdate) as o_year, l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount from PART, SUPPLIER, LINEITEM, PARTSUPP, ORDERS, NATION where s_suppkey = l_suppkey and ps_suppkey = l_suppkey and ps_partkey = l_partkey and p_partkey = l_partkey and o_orderkey = l_orderkey and s_nationkey = n_nationkey and p_name like '%dim%';
 /*****************/
 
 /* TPC-H Query 10 */
@@ -113,7 +119,8 @@ SELECT * FROM CUSTOMER, ORDERS, LINEITEM, NATION WHERE C_CUSTKEY = O_CUSTKEY AND
 /******************/
 
 /* TPC-H Query 11 */
-/* FIXME:( */
+/* Cant split easily */
+/* Multiple sub queries */
 select ps_partkey, sum(ps_supplycost * ps_availqty) as value from PARTSUPP, SUPPLIER, NATION where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'MOZAMBIQUE' group by ps_partkey having sum(ps_supplycost * ps_availqty) > (select sum(ps_supplycost * ps_availqty) * 0.0001000000 from PARTSUPP, SUPPLIER, NATION where ps_suppkey = s_suppkey and s_nationkey = n_nationkey and n_name = 'MOZAMBIQUE') order by value desc;
 
 /* Host Query: Q11 */
@@ -157,7 +164,8 @@ select * from LINEITEM, PART where l_partkey = p_partkey and l_shipdate >= '1996
 /********************/
 
 /* TPC-H Query: Q15 */
-/* FIXME :( */
+/* View query */
+/* Multiple sub queries */
 create view REVENUE0 (supplier_no, total_revenue) as select l_suppkey, sum(l_extendedprice * (1 - l_discount)) from LINEITEM where l_shipdate >= '1997-07-01' and l_shipdate < '1997-10-01' group by l_suppkey; select s_suppkey, s_name, s_address, s_phone, total_revenue from SUPPLIER, REVENUE0 where s_suppkey = supplier_no and total_revenue = ( select max(total_revenue) from REVENUE0) order by s_suppkey; drop view REVENUE0;
 
 /* Host Query: Q15 */
@@ -176,7 +184,7 @@ select * from PARTSUPP, PART where p_partkey = ps_partkey and p_brand <> 'Brand#
 /********************/
 
 /* TPC-H Query: Q17 */
-/* UNTESTED */
+/* Doesnt complete: 6709.514s */
 select sum(l_extendedprice) / 7.0 as avg_yearly from LINEITEM, PART where p_partkey = l_partkey and p_brand = 'Brand#44' and p_container = 'WRAP PKG' and l_quantity < (select 0.2 * avg(l_quantity) from LINEITEM where l_partkey = p_partkey);
 
 /* Host Query: Q17 */
@@ -209,7 +217,7 @@ select * from LINEITEM, PART where (p_partkey = l_partkey and p_brand = 'Brand#5
 /********************/
 
 /* TPC-H Query: Q20 */
-/* UNTESTED */
+/* Doesnt complete: 4401.108 */
 select s_name, s_address from SUPPLIER, NATION where s_suppkey in ( select ps_suppkey from PARTSUPP where ps_partkey in (select p_partkey from PART where p_name like 'green%') and ps_availqty > (select 0.5 * sum(l_quantity) from LINEITEM where l_partkey = ps_partkey and l_suppkey = ps_suppkey and l_shipdate >= '1993-01-01' and l_shipdate < '1994-01-01')) and s_nationkey = n_nationkey and n_name = 'ALGERIA' order by s_name;
 
 /* Host Query: Q20 */
@@ -231,7 +239,13 @@ select * from SUPPLIER, LINEITEM l1, ORDERS, NATION where s_suppkey = l1.l_suppk
 /*******************/
 
 /* TPC-H Query: Q22 */
-/* FIXME :(*/
-select cntrycode, count(*) as numcust, sum(c_acctbal) as totacctbal from (select substring(c_phone from 1 for 2) as cntrycode, c_acctbal from CUSTOMER where substring(c_phone from 1 for 2) in ('20', '40', '22', '30', '39', '42', '21') and c_acctbal > ( select avg(c_acctbal) from CUSTOMER where c_acctbal > 0.00 and substring(c_phone from 1 for 2) in ('20', '40', '22', '30', '39', '42', '21')) and not exists ( select * from ORDERS where o_custkey = c_custkey)) as custsale group by cntrycode order by cntrycode;
+/* /* Tested and works */*/
+select cntrycode, count(*) as numcust, sum(c_acctbal) as totacctbal from (select substr(c_phone, 1, 2) as cntrycode, c_acctbal from CUSTOMER where substr(c_phone, 1, 2) in ('20', '40', '22', '30', '39', '42', '21') and c_acctbal > ( select avg(c_acctbal) from CUSTOMER where c_acctbal > 0.00 and substr(c_phone, 1, 2) in ('20', '40', '22', '30', '39', '42', '21')) and not exists ( select * from ORDERS where o_custkey = c_custkey)) as custsale group by cntrycode order by cntrycode;
+
+/* Host Query: Q22 */
+select cntrycode, count(*) as numcust, sum(c_acctbal) as totacctbal from TABLE1 group by cntrycode order by cntrycode;
+
+/* ssd Query: Q22 */
+select substr(c_phone, 1, 2) as cntrycode, c_acctbal from CUSTOMER where substr(c_phone, 1, 2) in ('20', '40', '22', '30', '39', '42', '21') and c_acctbal > ( select avg(c_acctbal) from CUSTOMER where c_acctbal > 0.00 and substr(c_phone, 1, 2) in ('20', '40', '22', '30', '39', '42', '21')) and not exists ( select * from ORDERS where o_custkey = c_custkey);
 
 /********************/
