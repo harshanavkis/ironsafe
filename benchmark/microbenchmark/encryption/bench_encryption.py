81dc716cee89040ce40adfa8cc0bfc10b2efb8d9
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2020-09-01 08:27:06 +0100

    Encryption benchmark: automate end to end
    DM-Crypt vs Fresh-sqlite(based on sqlcipher)
    
    Signed-off-by: Harshavardhan Unnibhavi <harshanavkis@gmail.com>

--- benchmark/microbenchmark/encryption/bench_encryption.py
+++ benchmark/microbenchmark/encryption/bench_encryption.py
@@ -3,6 +3,12 @@ import subprocess
 import os
 from pathlib import Path
 from shutil import copyfile
+from collections import defaultdict
+from datetime import datetime
+import json
+import pandas as pd
+
+NOW = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 SRC_DIR = os.path.realpath("../../../")
 CURR_DIR = os.path.realpath(".")
@@ -14,89 +20,176 @@ MERK_FILE_NAME = "merkle-tree-{}.bin"
 ENC_IMAGE_NAME = "TPCH-DM-CRYPT-{}.img"
 KEYFILE        = "DM-CRYPT-KEY-{}.keyfile"
 
+TEST_QUERY = "select * from lineitem;"
+
 """
-	Environment variables:
-		- SCALE_FACTOR
+    Environment variables:
+        - SCALE_FACTOR
 """
 
 def run_local_proc(cmd):
-	proc = subprocess.run(cmd, stdout=stdout)
-	return proc
+    proc = subprocess.run(cmd, stdout=stdout)
+    return proc
 
 def setup_stuff():
-	try:
-		scale_factor = float(os.environ["SCALE_FACTOR"])
-	except Exception as e:
-		print("SCALE_FACTOR should be a number, preferably a float")
-		sys.exit(1)
+    try:
+        scale_factor = float(os.environ["SCALE_FACTOR"])
+    except Exception as e:
+        print("SCALE_FACTOR should be a number, preferably a float")
+        sys.exit(1)
 
-	db_file = Path(os.path.join(TPCH_DATA_DIR, DB_FILE_NAME.format(scale_factor)))
-	if not db_file.is_file():
-		os.chdir(f"{SRC_DIR}/tpch")
-		cmd = ["./create_db.sh", f"{scale_factor}"]
-		proc = run_local_proc(cmd)
-		proc.wait()
+    db_file = Path(os.path.join(TPCH_DATA_DIR, DB_FILE_NAME.format(scale_factor)))
+    if not db_file.is_file():
+        os.chdir(f"{SRC_DIR}/tpch")
+        cmd = ["./create_db.sh", f"{scale_factor}"]
+        proc = run_local_proc(cmd)
+        proc.wait()
 
-	img_file = Path(os.path.join(TPCH_DATA_DIR, TPCH-DM-CRYPT.format(scale_factor)))
-	if not img_file.is_file():
-		img_size = scale_factor*1.2*1.5
-		subprocess.run(
-				[
-					"dd",
-					"if=/dev/zero",
-					f"of={img_file}",
-					"bs=1M",
-					"count=0",
-					f"seek={img_size}"
-				]
-			)
+    img_file = Path(os.path.join(TPCH_DATA_DIR, ENC_IMAGE_NAME.format(scale_factor)))
+    if not img_file.is_file():
+        img_size = scale_factor*1.2*1.5
+        subprocess.run(
+                [
+                    "dd",
+                    "if=/dev/zero",
+                    f"of={str(img_file)}",
+                    "bs=1M",
+                    "count=0",
+                    f"seek={img_size}"
+                ]
+            )
 
-		subprocess.run(
-				[
-					"dd",
-					"if=/dev/urandom",
-					f"of={os.path.join(TPCH_DATA_DIR, KEYFILE.format(scale_factor))}",
-					"bs=1024",
-					"count=1"
-				]
-			)
+        subprocess.run(
+                [
+                    "dd",
+                    "if=/dev/urandom",
+                    f"of={os.path.join(TPCH_DATA_DIR, KEYFILE.format(scale_factor))}",
+                    "bs=1024",
+                    "count=1"
+                ]
+            )
 
-		subprocess.run(
-				[
-					"sudo",
-					"cryptsetup",
-					"luksFormat",
-					f"{img_file}",
-					f"{KEYFILE.format(scale_factor)}"
-				],
-				input="YES"
-			)
+        subprocess.run(
+                [
+                    "sudo",
+                    "cryptsetup",
+                    "luksFormat",
+                    f"{str(img_file)}",
+                    f"{KEYFILE.format(scale_factor)}"
+                ],
+                input="YES"
+            )
 
-		subprocess.run(
-				[
-					"sudo",
-					"cryptsetup",
-					"luksOpen",
-					f"{img_file}",
-					"benchEncryptVol",
-					"--key-file",
-					f"{KEYFILE.format(scale_factor)}"
-				]
-			)
+        subprocess.run(
+                [
+                    "sudo",
+                    "cryptsetup",
+                    "luksOpen",
+                    f"{str(img_file)}",
+                    "benchEncryptVol",
+                    "--key-file",
+                    f"{KEYFILE.format(scale_factor)}"
+                ]
+            )
 
-		subprocess.run(["sudo", "mkfs.ext4", "/dev/mapper/benchEncryptVol"])
-		subprocess.run(["sudo", "mount", "/dev/mapper/benchEncryptVol", "/mnt"])
-		subprocess.run(["sudo", "chown", "-R", "$USER", "/mnt"])
+        subprocess.run(["sudo", "mkfs.ext4", "/dev/mapper/benchEncryptVol"])
+        subprocess.run(["sudo", "mount", "/dev/mapper/benchEncryptVol", "/mnt"])
+        subprocess.run(["sudo", "chown", "-R", "$USER", "/mnt"])
 
-		copyfile(db_file, os.path.join("/mnt", DB_FILE_NAME.format(scale_factor)))
+        copyfile(db_file, os.path.join("/mnt", DB_FILE_NAME.format(scale_factor)))
 
-		subprocess.run(["sudo", "umount", "/mnt"])
-		subprocess.run(["sudo", "cryptsetup", "luksClose", "benchEncryptVol"])
+        subprocess.run(["sudo", "umount", "/mnt"])
+        subprocess.run(["sudo", "cryptsetup", "luksClose", "benchEncryptVol"])
 
-	os.chdir(CURR_DIR)
+    os.chdir(os.path.join(SRC_DIR, "fresh-sqlite"))
+    fresh_lite_env = os.environ.copy()
+    openssl_src = os.path.join(SRC_DIR, "openssl-src")
+    if not Path(os.path.join(openssl_src, "libcrypto.so.3")).is_file():
+        os.chdir(openssl_src)
+        subprocess.run(["./config"])
+        subprocess.run(["make"])
+        os.chdir(os.path.join(SRC_DIR, "fresh-sqlite"))
+
+    fresh_lite_env["OPENSSL_SRC"] = os.path.join(SRC_DIR, "openssl-src")
+    subprocess.run(["make", "hello-query"], env=fresh_lite_env)
+
+    os.chdir(CURR_DIR)
+
+def process_enc_bench_out(kind, stats, proc_out):
+    stats["kind"].append(kind)
+    for line in proc_out:
+        try:
+            data = json.loads(line)
+            for i in data:
+                stats[i].append(data[i])
+        except Exception as e:
+            print(f"{line}: not in JSON format")
+
+def bench_dm_crypt(kind, stats):
+    scale_factor = float(os.environ["SCALE_FACTOR"])
+    subprocess.run(
+                [
+                    "sudo",
+                    "cryptsetup",
+                    "luksOpen",
+                    f"{os.path.join(TPCH_DATA_DIR, ENC_IMAGE_NAME.format(scale_factor))}",
+                    "benchEncryptVol",
+                    "--key-file",
+                    f"{KEYFILE.format(scale_factor)}"
+                ]
+            )
+
+    subprocess.run(["sudo", "mount", "/dev/mapper/benchEncryptVol", "/mnt"])
+    subprocess.run(["sudo", "chown", "-R", "$USER", "/mnt"])
+
+    proc = subprocess.Popen(
+            [
+                os.path.join(SRC_DIR, "fresh-sqlite/hello-query"),
+                os.path.join(TPCH_DATA_DIR, MERK_FILE_NAME.format(scale_factor)),
+                os.path.join("/mnt", DB_FILE_NAME.format(scale_factor)),
+                "",
+                TEST_QUERY
+            ],
+            stdout=subprocess.PIPE,
+            text=True
+        )
+
+    process_enc_bench_out(kind, stats, proc.stdout)
+
+def bench_fresh_crypt(kind, stats):
+    proc = subprocess.Popen(
+            [
+                os.path.join(SRC_DIR, "fresh-sqlite/hello-query"),
+                os.path.join(TPCH_DATA_DIR, MERK_FILE_NAME.format(scale_factor)),
+                os.path.join(TPCH_DATA_DIR, FRESH_DB_NAME.format(scale_factor)),
+                "kun",
+                TEST_QUERY,
+            ],
+            stdout=subprocess.PIPE,
+            text=True
+        )
+
+    process_enc_bench_out(kind, stats, proc.stdout)
 
 def main():
-	pass
+    stats = defaultdict(list)
+
+    setup_stuff()
+
+    benchmarks = {
+        "dm-crypt": bench_dm_crypt,
+        "fresh-crypt": bench_fresh_crypt,
+    }
+
+    for name, benchmark in benchmarks.items():
+        benchmark(name, stats)
+
+    csv = f"encrypt-bench-{NOW}.csv"
+    print(csv)
+
+    df = pd.DataFrame(stats)
+    df.to_csv(csv, index=False)
+    df.to_csv("encrypt-bench-latest.csv", index=False)
 
 if __name__=="__main__":
-	main()
+    main()
