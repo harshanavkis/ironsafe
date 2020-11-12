ad042b2ad98218ac353dc09bcf15532ff0e0579e
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2020-11-12 17:27:25 +0000

    Update nvme setup

--- benchmark/setup_nvme_tcp_target.sh
+++ benchmark/setup_nvme_tcp_target.sh
@@ -4,18 +4,18 @@
 mkdir /sys/kernel/config/nvmet/subsystems/secndp
 cd /sys/kernel/config/nvmet/subsystems/secndp
 
-echo 1 | tee -a attr_allow_any_host
+echo 1 > attr_allow_any_host
 mkdir namespaces/1
 cd namespaces/1
-echo -n /dev/nvme0n1 | tee -a device_path
-echo 1 | tee -a enable
+echo -n /dev/nvme0n1 > device_path
+echo 1 > enable
 
 mkdir /sys/kernel/config/nvmet/ports/1
 cd /sys/kernel/config/nvmet/ports/1
-echo $1 | tee -a addr_traddr
-echo tcp | tee -a addr_trtype
+echo $1 > addr_traddr
+echo "tcp" > addr_trtype
 echo 4420 | tee -a addr_trsvcid
-echo ipv4 | tee -a addr_adrfam
+echo "ipv4" > addr_adrfam
 
 ln -s /sys/kernel/config/nvmet/subsystems/secndp /sys/kernel/config/nvmet/ports/1/subsystems/secndp
 
