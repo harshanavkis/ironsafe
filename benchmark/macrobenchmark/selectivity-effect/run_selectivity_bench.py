24f9cc14d2ccd74aa639bd279462ad63f6f44a14
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2020-12-09 17:41:33 +0000

    Automate selectivity bench

--- benchmark/macrobenchmark/selectivity-effect/run_selectivity_bench.py
+++ benchmark/macrobenchmark/selectivity-effect/run_selectivity_bench.py
@@ -1,7 +1,10 @@
 import sys
+sys.path.append("../../")
 import os
 import subprocess
+from collections import defaultdict
 from contextlib import contextmanager
+from datetime import datetime
 from pathlib import Path
 import time
 from io import StringIO
@@ -9,16 +12,17 @@ import json
 import pandas as pd
 
 from selectivity_split import determine_split
+from helpers import clear_cache
 
-import pdb
+NOW = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 stdout=subprocess.PIPE
 
-sql_query = "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice*(1 - l_discount)) as sum_disc_price, sum(l_extendedprice*(1 - l_discount)*(1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from LINEITEM where l_shipdate {};"
+sql_query = "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice*(1 - l_discount)) as sum_disc_price, sum(l_extendedprice*(1 - l_discount)*(1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from LINEITEM where l_shipdate <= {} group by l_returnflag, l_linestatus order by l_returnflag, l_linestatus;"
 
-host_query = "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice*(1 - l_discount)) as sum_disc_price, sum(l_extendedprice*(1 - l_discount)*(1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from TABLE1;"
+host_query = "select l_returnflag, l_linestatus, sum(l_quantity) as sum_qty, sum(l_extendedprice) as sum_base_price, sum(l_extendedprice*(1 - l_discount)) as sum_disc_price, sum(l_extendedprice*(1 - l_discount)*(1 + l_tax)) as sum_charge, avg(l_quantity) as avg_qty, avg(l_extendedprice) as avg_price, avg(l_discount) as avg_disc, count(*) as count_order from TABLE1 group by l_returnflag, l_linestatus order by l_returnflag, l_linestatus;"
 
-ssd_query = "select l_returnflag, l_linestatus,l_quantity, l_extendedprice, l_discount, l_tax from LINEITEM where l_shipdate {};"
+ssd_query = "select l_returnflag, l_linestatus,l_quantity, l_extendedprice, l_discount, l_tax from LINEITEM where l_shipdate <= '1998-08-15';"
 
 device_host_query = "select l_returnflag, l_linestatus, sum_qty, sum_base_price, sum_disc_price, sum_charge, avg_qty, avg_price, avg_disc, count_order from TABLE1;"
 
@@ -34,256 +38,304 @@ FRESH_DB_NAME  = "TPCH-{}-fresh-enc.db"
 MERK_FILE_NAME = "merkle-tree-{}.bin"
 
 """
-	Environment variables:
-		- REMOTE_SRC
-		- STORAGE_SERVER_IP
-		- REMOTE_USER
-		- SCALE_FACTORS
-		- SPLIT_POINTS
+    Environment variables:
+        - REMOTE_SRC
+        - STORAGE_SERVER_IP
+        - REMTOE_NIC_IP
+        - REMOTE_USER
+        - SCALE_FACTORS
+        - SPLIT_POINTS
 """
 def process_host_ndp_output(res):
-	return res
+    return res
 
 def run_local_proc(cmd, env=None):
-	proc = subprocess.run(cmd, stdout=stdout, env=env)
-	return proc
+    proc = subprocess.run(cmd, stdout=stdout, env=env)
+    return proc
 
 def setup_exp():
-	os.chdir(f"{ROOT_DIR}/host/non-secure/")
-	cmd = ["make"]
-	proc = run_local_proc(cmd)
-	# proc.wait()
+    os.chdir(f"{ROOT_DIR}/host/non-secure/")
+    cmd = ["make"]
+    proc = run_local_proc(cmd)
+    # proc.wait()
 
-	os.chdir(f"{ROOT_DIR}/openssl-src/")
-	cmd = ["./Configure"]
-	proc = run_local_proc(cmd)
-	# proc.wait()
+    os.chdir(f"{ROOT_DIR}/openssl-src/")
+    cmd = ["./Configure"]
+    proc = run_local_proc(cmd)
+    # proc.wait()
 
-	cmd = ["make"]
-	proc = run_local_proc(cmd)
-	# proc.wait()
+    cmd = ["make"]
+    proc = run_local_proc(cmd)
+    # proc.wait()
 
-	cmd = ["docker", "image", "inspect", "host-ndp:latest"]
-	proc = run_local_proc(cmd)
-	# proc.wait()
+    cmd = ["docker", "image", "inspect", "host-ndp:latest"]
+    proc = run_local_proc(cmd)
+    # proc.wait()
 
-	os.chdir(ROOT_DIR)
-	if proc.returncode == 1:
-		cmd = ["docker", "build", "-f", f"{ROOT_DIR}/benchmark/scone-stuff/sec-ndp", "-t", "host-ndp", f"{ROOT_DIR}/"]
-		run_local_proc(cmd)
-		# proc.wait()
+    os.chdir(ROOT_DIR)
+    if proc.returncode == 1:
+        cmd = ["docker", "build", "-f", f"{ROOT_DIR}/benchmark/scone-stuff/sec-ndp", "-t", "host-ndp", f"{ROOT_DIR}/"]
+        run_local_proc(cmd)
+        # proc.wait()
 
-	cmd = ["docker", "image", "inspect", "pure-host-sec:latest"]
-	proc = run_local_proc(cmd)
-	# proc.wait()
+    cmd = ["docker", "image", "inspect", "pure-host-sec:latest"]
+    proc = run_local_proc(cmd)
+    # proc.wait()
 
-	if proc.returncode == 1:
-		cmd = ["docker", "build", "-f", f"{ROOT_DIR}/benchmark/scone-stuff/pure-host-sec", "-t", "pure-host-sec", f"{ROOT_DIR}/"]
-		run_local_proc(cmd)
-		# proc.wait()
+    if proc.returncode == 1:
+        cmd = ["docker", "build", "-f", f"{ROOT_DIR}/benchmark/scone-stuff/pure-host-sec", "-t", "pure-host-sec", f"{ROOT_DIR}/"]
+        run_local_proc(cmd)
+        # proc.wait()
 
-	os.chdir(f"{ROOT_DIR}/fresh-sqlite")
-	make_env = os.environ.copy()
-	make_env["SCONE"] = "false"
-	make_env["OPENSSL_SRC"] = f"{ROOT_DIR}/openssl-src"	
-	cmd = ["make", "clean"]
-	run_local_proc(cmd)
-	cmd = ["make", "hello-query"]
-	run_local_proc(cmd, make_env)
+    os.chdir(f"{ROOT_DIR}/fresh-sqlite")
+    make_env = os.environ.copy()
+    make_env["SCONE"] = "false"
+    make_env["OPENSSL_SRC"] = f"{ROOT_DIR}/openssl-src" 
+    cmd = ["make", "clean"]
+    run_local_proc(cmd)
+    cmd = ["make", "hello-query"]
+    run_local_proc(cmd, make_env)
 
-	# TODO: Correctly setup nvme over tcp and mount drive
-	NVME_TCP_DIR = os.path.join(ROOT_DIR, "tpch/build")
+    # TODO: Correctly setup nvme over tcp and mount drive
+    NVME_TCP_DIR = os.path.join(ROOT_DIR, "tpch/build")
 
-	os.chdir(CURR_DIR)
+    os.chdir(CURR_DIR)
 
 def process_pure_host_output(proc_stdout):
-	try:
-		data = json.loads(proc_stdout.rstrip())
-		data = pd.DataFrame(data, index=[0])
-		print(data)
-		return data
-	except Exception as e:
-		print("Unable to decode json...")
-		sys.exit(1)
+    try:
+        data = json.loads(proc_stdout.rstrip())
+        data = pd.DataFrame(data, index=[0])
+        print(data)
+        return data["query_exec_time"]
+    except Exception as e:
+        print("Unable to decode json...")
+        sys.exit(1)
 
 def run_pure_host_non_secure(cq, db_file, scale_factor):
-	local_cmd = [
-		"./run_pure_host_non_secure.sh",
-		os.path.join(os.path.join(ROOT_DIR, "tpch/build"), MERK_FILE_NAME.format(scale_factor)),
-		os.path.join(os.path.join(ROOT_DIR, "tpch/build"), DB_FILE_NAME.format(scale_factor)),
-		f"{cq}"
-	]
+    try:
+        data_dir = os.environ["NVME_TCP_DIR"]
+    except Exception as e:
+        print("Provide NVME_TCP_DIR env var")
+        sys.exit(1)
 
-	print(local_cmd)
+    cmd = [
+        "docker",
+        "run",
+        "--mount",
+        f"type=bind,source={data_dir},target=/data",
+        "pure-host",
+        "/bin/bash",
+        "-c",
+        "./hello-query {} {} \"\" \"{}\"".format(os.path.join(data_dir, MERK_FILE_NAME.format(scale_factor)), os.path.join(data_dir, DB_FILE_NAME.format(scale_factor)), cq.replace("'", "'\\''"))
+    ]
 
-	proc = subprocess.run(local_cmd, stdout=stdout, text=True)
-	return process_pure_host_output(proc.stdout)
+    print(cmd)
+
+    proc = subprocess.run(cmd, stdout=stdout, text=True)
+    return process_pure_host_output(proc.stdout)
 
 def run_pure_host_secure(cq, db_file, scale_factor):
-	local_cmd = [
-		"docker",
-		"run",
-		"--mount",
-		f"type=bind,source={ROOT_DIR}/tpch/build,target=/data",
-		"pure-host-sec",
-		"/bin/bash",
-		"-c",
-		"SCONE_VERSION=1 SCONE_HEAP=2G ./hello-query /data/{} /data/{} kun \"{}\"".format(MERK_FILE_NAME.format(scale_factor), FRESH_DB_NAME.format(scale_factor), cq)
-	]
-	print(local_cmd)
+    try:
+        data_dir = os.environ["NVME_TCP_DIR"]
+    except Exception as e:
+        print("Provide NVME_TCP_DIR env var")
+        sys.exit(1)
 
-	proc = subprocess.run(local_cmd, stdout=stdout, text=True)
-	return process_pure_host_output(proc.stdout.rstrip())
+    cmd = [
+        "docker",
+        "run",
+        "--mount",
+        f"type=bind,source={data_dir},target=/data",
+        "pure-host-sec",
+        "/bin/bash",
+        "-c",
+        "SCONE_VERSION=1 SCONE_HEAP=4G ./hello-query {} {} kun \"{}\"".format(os.path.join(data_dir, MERK_FILE_NAME.format(scale_factor)), os.path.join(data_dir, DB_FILE_NAME.format(scale_factor)), cq.replace("'", "'\\''"))
+    ]
+    print(cmd)
+
+    proc = subprocess.run(local_cmd, stdout=stdout, text=True)
+    return process_pure_host_output(proc.stdout.rstrip())
 
 def run_vanilla_ndp_non_secure(hq, sq, db_file, scale_factor):
-	proc = subprocess.run(["./selectivity_ndp.sh", f"{scale_factor}", "non-secure", hq, sq], stdout=stdout, text=True)
-	# proc.wait()
+    env_var = os.environ.copy()
+    env_var["CONN_TYPE"] = "non-secure"
+    env_var["OFFLOAD_TYPE"] = "split-comp"
+    env_var["DATE"] = f"{NOW}"
 
-	kill_proc = subprocess.run(["./kill_rem_process.sh"], env=os.environ)
+    init_cmd = [
+        './run_macrobench_host.sh'
+    ]
+    storage_proc = subprocess.Popen(init_cmd, stdout=subprocess.PIPE, env=env_var)
+    storage_proc.wait()
+    time.sleep(10)
 
-	return process_host_ndp_output(proc.stdout.rstrip())
+    rem_ip   = os.environ["REMOTE_NIC_IP"]
+    local_cmd = [
+        "docker",
+        "run",
+        "vanilla-ndp",
+        "/bin/bash",
+        "-c",
+        "./host-ndp -D dummy -Q \"{}\" -S \"{}\" {}".format(hq.replace("'", "'\\''"), sq.replace("'", "'\\''"), os.environ["REMOTE_NIC_IP"])
+    ]
+
+    local_proc = subprocess.Popen(local_cmd, stdout=subprocess.PIPE, env=env_var, text=True, stderr=subprocess.PIPE)
+    while True:
+            local_proc.wait()
+            if local_proc.returncode !=0:
+                continue
+            else:
+                break
+    query_res = local_proc.stdout.read().strip().split(',')
+
+    return float(query_res[0].strip())
 
 
 def run_secure_ndp_secure(hq, sq, scale_factor):
-	remote_proc = subprocess.run(["./selectivity_sec_ndp.sh", f"{scale_factor}", "secure"], stdout=stdout, text=True)
+    env_var = os.environ.copy()
+    env_var["CONN_TYPE"] = "secure"
+    env_var["OFFLOAD_TYPE"] = "split-comp"
+    env_var["DATE"] = NOW
 
-	time.sleep(5)
-	remote_ip = os.environ["STORAGE_SERVER_IP"]
-	if remote_ip == "127.0.0.1":
-		remote_ip = "172.17.0.1"
+    init_cmd = [
+        './run_macrobench_host.sh'
+    ]
+    storage_proc = subprocess.Popen(init_cmd, stdout=subprocess.PIPE, env=env_var)
+    storage_proc.wait()
 
-	local_cmd = ["docker", "run", "host-ndp", "/bin/bash", "-c", "cd /sqlite-ndp/host/secure/ && SCONE_VERSION=1 SCONE_HEAP=2G ./host-ndp -D .. -Q \"{}\" -S \"{}\" {}".format(hq, sq, remote_ip)]
-	local_proc = subprocess.Popen(local_cmd, stdout=stdout, text=True)
-	local_proc.wait()
+    time.sleep(10)
 
-	kill_proc = subprocess.run(["./kill_rem_process.sh"], env=os.environ)
+    local_cmd = [
+        "docker",
+        "run",
+        "--device=/dev/isgx",
+        "host-ndp",
+        "/bin/bash",
+        "-c",
+        "SCONE_VERSION=1 SCONE_HEAP=4G ./host-ndp -D dummy -Q \"{}\" -S \"{}\" {}".format(hq.replace("'", "'\\''"), sq.replace("'", "'\\''"), os.environ["REMOTE_NIC_IP"])
+    ]
 
-	return process_host_ndp_output(local_proc.stdout.read())
+    local_proc = subprocess.Popen(local_cmd, stdout=subprocess.PIPE, env=env_var, text=True)
+    while True:
+        local_proc.wait()
+        #import pdb; pdb.set_trace()
+        if local_proc.returncode !=0:
+            #print(local_proc.returncode)
+            continue
+        else:
+            break
+    query_res = local_proc.stdout.read().strip().split(',')
 
-def run_secure_device_only(dhq, dsq, scale_factor):
-	remote_proc = subprocess.run(["./selectivity_sec_ndp.sh", f"{scale_factor}", "secure"], stdout=stdout, text=True)
+    return float(query_res[0].strip())
 
-	time.sleep(5)
-	remote_ip = os.environ["STORAGE_SERVER_IP"]
-	if remote_ip == "127.0.0.1":
-		remote_ip = "172.17.0.1"
+def run_secure_device_only(cq, scale_factor):
+    rem_src = os.environ["REMOTE_SRC"]
+    rem_user = os.environ["REMOTE_USER"]
 
-	local_cmd = ["docker", "run", "host-ndp", "/bin/bash", "-c", "cd /sqlite-ndp/host/secure/ && SCONE_VERSION=1 SCONE_HEAP=2G ./host-ndp -D .. -Q \"{}\" -S \"{}\" {}".format(dhq, dsq, remote_ip)]
-	local_proc = subprocess.Popen(local_cmd, stdout=stdout, text=True)
-	local_proc.wait()
+    remote_ip = os.environ["STORAGE_SERVER_IP"]
+    if remote_ip == "127.0.0.1":
+        remote_ip = "172.17.0.1"
 
-	kill_proc = subprocess.run(["./kill_rem_process.sh"], env=os.environ)
+    merk_file = os.path.join(REMOTE_SRC, f"tpch/build/merkle-tree-{scale_factor}.bin")
+    db_file   = os.path.join(REMOTE_SRC, f"tpch/build/TPCH-{scale_factor}-fresh-enc.db")
 
-	return process_host_ndp_output(local_proc.stdout.read())
+    rem_cmd = [
+        os.path.join(rem_src, "fresh-sqlite/hello-query"),
+        merk_file,
+        db_file,
+        "kun"
+    ]
 
-def run_all_configs(cq, hq, sq, dhq, dsq, db_file, scale_factor):
-	print("Running pure host non-secure...")
-	phns = run_pure_host_non_secure(cq, db_file, scale_factor)
+    rem_cmd = " ".join(rem_cmd)
 
-	print("Running pure host secure...")
-	phs  = run_pure_host_secure(cq, db_file, scale_factor)
+    ssh_cmd = f"ssh {rem_user}@{remote_ip} \"{rem_cmd} \"{cq}\"\""
+    proc = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, text=True)
+    proc.wait()
 
-	print("Running vanilla ndp non-secure...")
-	vnns = run_vanilla_ndp_non_secure(hq, sq, db_file, scale_factor)
+    return process_pure_host_output(proc.stdout)
 
-	print("Running secure ndp...")
-	sns  = run_secure_ndp_secure(hq, sq, scale_factor)
+def run_all_configs(cq, hq, sq, dhq, dsq, db_file, scale_factor, stats):
+    print("Running pure host non-secure...")
+    phns = run_pure_host_non_secure(cq, db_file, scale_factor)
+    stats["system"].append("phns")
+    stats["time"].append(phns)
 
-	print("Running purely on storage server...")
-	sss  = run_secure_device_only(dhq, dsq, scale_factor)
+    print("Running pure host secure...")
+    phs  = run_pure_host_secure(cq, db_file, scale_factor)
+    stats["system"].append("phs")
+    stats["time"].append(phs)
 
-	return [phns, phs, vnns, sns, sss]
+    print("Running vanilla ndp non-secure...")
+    vnns = run_vanilla_ndp_non_secure(hq, sq, db_file, scale_factor)
+    stats["system"].append("vnns")
+    stats["time"].append(vnns)
 
-def run_bench(scale_factor, split_point):
-	db_file = DB_DIR.format(scale_factor)
-	if not os.path.isfile(db_file):
-		print(f"{db_file} does not exist")
-		return
+    print("Running secure ndp...")
+    sns  = run_secure_ndp_secure(hq, sq, scale_factor)
+    stats["system"].append("sns")
+    stats["time"].append(sns)
 
-	result_dict = {}
+    print("Running purely on storage server...")
+    sss  = run_secure_device_only(cq, scale_factor)
+    stats["system"].append("sss")
+    stats["time"].append(sss)
 
-	# split_point*total_rows < split_date
-	split_date = determine_split(db_file, split_point, "L_SHIPDATE")
+def run_bench(scale_factor, split_point, stats):
+    db_file = DB_DIR.format(scale_factor)
+    if not os.path.isfile(db_file):
+        print(f"{db_file} does not exist")
+        return
 
-	upper_sel  = split_point
-	lower_sel  = (1 - split_point)
+    stats["scale_factor"].append(scale_factor)
+    stats["split_point"].append(split_point)
 
-	result_dict[upper_sel] = None
-	result_dict[lower_sel] = None
+    # split_point*total_rows < split_date
+    split_date = determine_split(db_file, split_point, "L_SHIPDATE")
 
-	result_dict[upper_sel] = run_all_configs(
-		sql_query.format("<={}".format(split_date)),
-		host_query,
-		ssd_query.format("<={}".format(split_date)),
-		device_host_query,
-		device_ssd_query.format("<={}".format(split_date)),
-		db_file,
-		scale_factor
-	)
+    stats["split_date"].append(split_date)
 
-	if upper_sel != lower_sel:
-		result_dict[lower_sel] = run_all_configs(
-			sql_query.format(">{}".format(split_date)),
-			host_query,
-			device_host_query,
-			device_ssd_query.format("<={}".format(split_date)),
-			ssd_query.format(">{}".format(split_date)),
-			db_file,
-			scale_factor
-		)
+    upper_sel  = split_point
 
-	return result_dict
+    run_all_configs(
+        sql_query.format("<={}".format(split_date)),
+        host_query,
+        ssd_query.format("<={}".format(split_date)),
+        device_host_query,
+        device_ssd_query.format("<={}".format(split_date)),
+        db_file,
+        scale_factor,
+        stats
+    )
 
-def save_to_csv(result_dict, scale_factor, f_names):
-	for selectivity in result_dict:
-		temp_f_names = [i.format(selectivity, scale_factor) for i in f_names]
-		data = result_dict[selectivity][:2]
-		for i in range(len(data)):
-			result_dict[selectivity][i].to_csv(temp_f_names[i], index=False)
-
-
-def result_to_csv(result_dict, scale_factor, f_names):
-	for selectivity in result_dict:
-		temp_f_names = [i.format(selectivity, scale_factor) for i in f_names]
-		sel_res = result_dict[selectivity][2:]
-
-		for i in range(len(sel_res)):
-			csv_data = StringIO(sel_res[i])
-			df = pd.read_csv(csv_data, sep=',', header=None)
-			df.to_csv(temp_f_names[i], header=None, index=False)		
+    return result_dict
 
 def main():
-	scale_factors = os.environ['SCALE_FACTORS']
-	split_points  = os.environ['SPLIT_POINTS']
+    scale_factors = os.environ['SCALE_FACTORS']
+    split_points  = os.environ['SPLIT_POINTS']
 
-	if not scale_factors:
-		print("Provide SCALE_FACTORS env var")
-		sys.exit(1)
-	if not split_points:
-		print("Provide SPLIT_POINTS env var")
+    if not scale_factors:
+        print("Provide SCALE_FACTORS env var")
+        sys.exit(1)
+    if not split_points:
+        print("Provide SPLIT_POINTS env var")
 
-	scale_factors = scale_factors.split(" ")
-	scale_factors = [float(i) for i in scale_factors]
+    scale_factors = scale_factors.split(" ")
+    scale_factors = [int(i) for i in scale_factors]
 
-	split_points  = split_points.split(" ")
-	split_points  = [float(i) for i in split_points]
+    split_points  = split_points.split(" ")
+    split_points  = [float(i) for i in split_points]
 
-	setup_exp()
+    # setup_exp()
 
-	res_f_names = [
-		"phns-{}-scale-{}.csv",
-		"phs-{}-scale-{}.csv",
-		"vnns-{}-scale-{}.csv",
-		"sns-{}-scale-{}.csv",
-		"sss-{}-scale-{}.csv"
-	]	
+    stats = defaultdict(list)
 
-	for sf in scale_factors:
-		for sp in split_points:
-			result_dict = run_bench(sf, sp)
-			result_to_csv(result_dict, sf, res_f_names[2:])
-			save_to_csv(result_dict, sf, res_f_names[:2])
+    for sf in scale_factors:
+        for sp in split_points:
+            result_dict = run_bench(sf, sp, stats)
+
+    df = pd.DataFrame(stats)
+    df.to_csv(f"selectivity-effect-bench-{NOW}.csv")
 
 
 if __name__=="__main__":
-	main()
+    main()
