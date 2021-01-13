cfb120f09c2a055919813039b5a12f2ca3a49ea2
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2021-01-13 10:56:20 +0000

    Update plots

--- plots/plot.py
+++ plots/plot.py
@@ -1,11 +1,13 @@
 import sys
 import seaborn as sns
 import pandas as pd
+import matplotlib
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 import matplotlib.ticker as ticker
 from graph_utils import apply_aliases, column_alias, systems_order, change_width, config_order
 import numpy as np
+from scipy import stats
 
 """
     - first arg: kind of plot(ndp, microbench)
@@ -27,7 +29,8 @@ mpl.rcParams["ps.fonttype"] = 42
 # sns.set_context("paper", rc={"font.size":10,"axes.titlesize":10,"axes.labelsize":10})
 # sns.set_palette(sns.color_palette(palette="gray", n_colors=2))
 
-plot_queries = [1, 11, 15, 17, 20, 22]
+# plot_queries = [1, 6, 11, 13, 15, 16, 17, 18, 20, 21, 22]
+plot_queries = []
 storage_side_secndp_cols = [
     "num_prot_pages",
     "query_exec_time",
@@ -120,11 +123,11 @@ def secndp_overheads():
     stsn["query"] = vn["query"]
     stsn = stsn[["query", "query_exec_time", "codec_time", "mt_verify_time"]]
 
-    columns = ["query", "ndp", "other", "enc", "freshness"]
+    columns = ["query", "ndp", "other", "encryption", "freshness"]
     plot_df = pd.DataFrame(columns=columns)
     plot_df["query"] = vn["query"]
     plot_df["freshness"] = stsn["mt_verify_time"]/sn["total_time"]
-    plot_df["enc"] = (stsn["codec_time"]-stsn["mt_verify_time"])/sn["total_time"]
+    plot_df["encryption"] = (stsn["codec_time"]-stsn["mt_verify_time"])/sn["total_time"]
     plot_df["other"] = (sn["total_time"]-stsn["query_exec_time"]-sn["total_host_query_time"])/sn["total_time"]
     plot_df["ndp"] = ((stsn["query_exec_time"]+sn["total_host_query_time"]-stsn["codec_time"])/sn["total_time"])
 
@@ -137,11 +140,13 @@ def secndp_overheads():
     # import pdb; pdb.set_trace()
 
     # sns.mpl.rc("figure", figsize=(8, 2))
-    ax = plot_df.plot.bar(stacked=True, rot=0, color={"freshness":"black", "enc":"dimgray", "other":"darkgray", "ndp":"lightgray"}, width=0.25, figsize=(8, 2))
+    ax = plot_df.plot.bar(stacked=True, rot=0, color={"freshness":"black", "encryption":"dimgray", "other":"darkgray", "ndp":"lightgray"}, width=0.5, figsize=(5, 2.2))
     ax.legend(loc="upper center", ncol=len(plot_df.columns), bbox_to_anchor=(0.5, 1.2), fontsize=leg_size)
     ax.tick_params(axis='both', which='major', labelsize=tick_fsize)
     ax.set_xlabel(xlabel="Query", fontsize=xlabel_fsize)
     ax.set_ylabel(ylabel="Overhead", fontsize=ylabel_fsize)
+
+    plt.grid(which="major", axis="y")
 
     plt.savefig("END_END_OVERHEAD.pdf", bbox_inches = 'tight', pad_inches = 0.1)
 
@@ -221,30 +226,41 @@ def end_end_rel_ndp():
     s_plot_df["query"] = phs_df["query"]
     s_plot_df["speedup"] = phs_df["Time [s]"]/sn["Time [s]"]
 
-    crap_df = [
-                ["non-secure", 1, 1], ["secure", 1, 1],
-                ["non-secure", 11,1], ["secure", 11, 1],
-                ["non-secure", 15, 1], ["secure", 15, 1],
-                ["non-secure", 17, 1], ["secure", 17, 1],
-                ["non-secure", 20, 1], ["secure", 20, 1],
-                ["non-secure", 22, 1], ["secure", 22, 1],
-            ]
-    crap_df = pd.DataFrame(crap_df)
-    crap_df.columns = ["kind", "query", "speedup"]
+    # plot_queries = [1, 6, 11, 13, 15, 16, 17, 18, 20, 21, 22]
+
+    # crap_df = [
+    #             ["non-secure", 1, 1], ["secure", 1, 1],
+    #             ["non-secure", 6, 1], ["secure", 6, 1],
+    #             ["non-secure", 11,1], ["secure", 11, 1],
+    #             ["non-secure", 13, 1], ["secure", 13, 1],
+    #             ["non-secure", 15, 1], ["secure", 15, 1],
+    #             ["non-secure", 16, 1], ["secure", 16, 1],
+    #             ["non-secure", 17, 1], ["secure", 17, 1],
+    #             ["non-secure", 18, 1], ["secure", 18, 1],
+    #             ["non-secure", 20, 1], ["secure", 20, 1],
+    #             ["non-secure", 21, 1], ["secure", 21, 1],
+    #             ["non-secure", 22, 1], ["secure", 22, 1],
+    #         ]
+    # crap_df = pd.DataFrame(crap_df)
+    # crap_df.columns = ["kind", "query", "speedup"]
 
     plot_df = pd.concat([ns_plot_df, s_plot_df])
 
     plot_df = plot_df[~plot_df["query"].isin(plot_queries)].reset_index()
     plot_df = plot_df.drop("index", axis=1)
 
-    plot_df = pd.concat([plot_df, crap_df])
+    # plot_df = pd.concat([plot_df, crap_df])
     plot_df = plot_df.sort_values("query")
+    # plot_df = plot_df.astype({plot_df.columns[1]:str, plot_df.columns[2]:np.float64})
+    non_sec = plot_df[plot_df[plot_df.columns[0]]=="non-secure"]
+    sec = plot_df[plot_df[plot_df.columns[0]]=="secure"]
+    average_row = [["non-secure", 0, stats.gmean(non_sec["speedup"])], ["secure", 0, stats.gmean(sec["speedup"])]]
+    plot_df = plot_df.append(pd.DataFrame(average_row, columns=plot_df.columns), ignore_index=True)
+    # import pdb; pdb.set_trace()
 
     # plot_df = apply_aliases(plot_df)
 
-    import pdb; pdb.set_trace()
-
-    sns.mpl.rc("figure", figsize=(8, 2))
+    sns.mpl.rc("figure", figsize=(16, 2))
 
     g = sns.barplot(
             data=plot_df,
@@ -257,31 +273,61 @@ def end_end_rel_ndp():
         )
     # g.fig.set_figheight(8)
     # g.fig.set_figwidth(16)
-    g.tick_params(axis='both', which='major', labelsize=tick_fsize)
-    g.set_xlabel(xlabel=column_alias(plot_df.columns[1]), fontsize=xlabel_fsize)
-    g.set_ylabel(ylabel=column_alias(plot_df.columns[2]), fontsize=ylabel_fsize)
-    g.legend(loc="upper center", fontsize=leg_size, bbox_to_anchor=(0.5, 1.2), ncol=2)
+    g.tick_params(axis='both', which='major', labelsize=tick_fsize*1.3)
+    g.set_xlabel(xlabel=column_alias(plot_df.columns[1]), fontsize=xlabel_fsize*1.5)
+    g.set_ylabel(ylabel=column_alias(plot_df.columns[2]), fontsize=ylabel_fsize*1.5)
+    g.legend(loc="upper center", fontsize=leg_size*1.3, bbox_to_anchor=(0.5, 1.25), ncol=2)
+    change_width(g, 0.2)
 
+    end_end_rel_ylim = 100
+    # g.set_ylim(top=end_end_rel_ylim)
     xticks = g.get_xticks()
 
-    bar_text = {
-        1 : "ndp incompatible",
-        11 : "ndp incompatible",
-        15 : "ndp incompatible",
-        17 : "ndp incompatible",
-        20 : "ndp incompatible",
-        22: "ndp incompatible",
-    }
+    # bar_text = {
+    #     1 : "ndp incompatible",
+    #     6 : "ndp incompatible",
+    #     11 : "ndp incompatible",
+    #     13 : "ndp incompatible",
+    #     15 : "ndp incompatible",
+    #     16 : "ndp incompatible",
+    #     17 : "ndp incompatible",
+    #     18 : "ndp incompatible",
+    #     20 : "ndp incompatible",
+    #     21 : "ndp incompatible",
+    #     22: "ndp incompatible",
+    # }
 
-    for i in range(len(xticks)):
-        if i+1 in plot_queries:
-            g.annotate(bar_text[i+1], (g.patches[i].get_x() + g.patches[i].get_width(), 1.2),
-                ha='center', fontsize=leg_size, color='k', rotation=90)
+    bar_height = list(plot_df["speedup"].values)
+    # import pdb; pdb.set_trace()
+    for i in range(len(g.patches)):
+        # if i+1 in plot_queries:
+        if g.patches[i].get_height() >=end_end_rel_ylim:
+            y_pos = 15
+        else:
+            y_pos = g.patches[i].get_height()
+        g.annotate(f"{g.patches[i].get_height():.1f}x", (g.patches[i].get_x() - 0.01 + g.patches[i].get_width() / 2., y_pos+0.1),
+            ha='center', va="bottom", fontsize=leg_size, color='k', annotation_clip=False)
+            # pass
+        # else:
+        #     if bar_height[i*2] > end_end_rel_ylim:
+        #         g.annotate(bar_height[i*2], (g.patches[i*2].get_x() + g.patches[i*2].get_width() / 2., 1.2),
+        #         ha='center', fontsize=leg_size, color='k', rotation=90)
+        #     if bar_height[i*2 + 1] > end_end_rel_ylim:
+        #         g.annotate(bar_height[i*2 + 1], (g.patches[i*2 + 1].get_x() + g.patches[i*2 + 1].get_width() / 2., 1.2),
+        #         ha='center', fontsize=leg_size, color='k', rotation=90)
+
+    x_labs = g.get_xticklabels()
+    x_labs[0] = 'AVG'
+    g.set_xticklabels(x_labs)
+    g.set_yscale("symlog")
+    g.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+
+    plt.grid(which="major", axis="y")
 
     plt.savefig("END_2_END_REL.pdf", bbox_inches = 'tight', pad_inches = 0.1)
 
 def ssd_sec_storage_overheads():
-    sec_store_queries = [9, 13]
+    sec_store_queries = [2, 9]
     s_ao_df = pd.read_csv(storage_all_offload, header=None)
     s_ao_df.columns = storage_side_secndp_cols
 
@@ -306,6 +352,7 @@ def ssd_sec_storage_overheads():
     plot_df = pd.concat([codec_df, mt_df])
     plot_df = plot_df[plot_df["query"].isin(sec_store_queries)].reset_index()
     plot_df = plot_df.drop("index", axis=1)
+    import pdb; pdb.set_trace()
     g = catplot(
             data=plot_df,
             x=plot_df.columns[0],
@@ -324,6 +371,8 @@ def ssd_sec_storage_overheads():
     g.ax.set_ylabel(ylabel=column_alias(plot_df.columns[1]), fontsize=ylabel_fsize*2)
     g.ax.legend(loc="upper center", fontsize=leg_size*2, ncol=2, bbox_to_anchor=(0.5, 1.1))
     g.ax.set(ylim=(0, 1))
+
+    plt.grid(which="major", axis="y")
 
     g.savefig("SEC_STORAGE.pdf")
 
@@ -364,10 +413,12 @@ def selectivity_vs_query():
     g.ax.set_ylabel(ylabel=column_alias("time"), fontsize=ylabel_fsize*2)
     g.ax.legend(loc="upper center", fontsize=leg_size*2, ncol=3, bbox_to_anchor=(0.5, 1.1))
 
+    plt.grid(which="major", axis="y")
+
     g.savefig("SELECTIVITY_VS_QUERY.pdf")
 
 def size_vs_query():
-    split_point = 0.01
+    split_point = 0.2
     data_size = [3, 4, 5]
 
     dfs = sys.argv[2:]
@@ -405,7 +456,60 @@ def size_vs_query():
     g.ax.set_ylabel(ylabel=column_alias("time"), fontsize=ylabel_fsize*2)
     g.ax.legend(loc="upper center", fontsize=leg_size*2, ncol=3, bbox_to_anchor=(0.5, 1.1))
 
+    plt.grid(which="major", axis="y")
+
     g.savefig("SIZE_VS_QUERY.pdf")
+
+def io_speedup():
+    df = pd.read_csv(sys.argv[2], header=0)
+    plot_df = df[["Query", "I/O Ratio"]]
+    plot_df = plot_df[~plot_df["Query"].isin(plot_queries)].reset_index()
+    plot_df = plot_df.drop("index", axis=1)
+    plot_df["I/O Ratio"] = plot_df["I/O Ratio"]
+    plot_df.columns = ["Query", "I/O Ratio"]
+
+    average_row = [[0, stats.gmean(plot_df["I/O Ratio"])]]
+    plot_df = plot_df.append(pd.DataFrame(average_row, columns=plot_df.columns), ignore_index=True)
+
+    # sns.mpl.rc("figure", figsize=(2, 8))
+
+    g = catplot(
+            data=plot_df,
+            x=plot_df.columns[0],
+            y=plot_df.columns[1],
+            legend=False,
+            palette = ["black"],
+            kind="bar"
+        )
+    g.ax.set_yscale("symlog")
+    g.ax.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    # plt.autoscale(enable=True, axis='y')
+    g.fig.set_figheight(2)
+    g.fig.set_figwidth(5)
+    change_width(g.ax, 0.5)
+
+    x_labs = g.ax.get_xticklabels()
+    x_labs[0] = 'AVG'
+    g.ax.set_xticklabels(x_labs)
+
+    # y_labs = g.ax.get_yticklabels()
+    # y_labs = [int(float(i.get_text())) for i in y_labs]
+    # g.ax.set_yticklabels(y_labs)
+
+    g.ax.tick_params(axis='both', which='major', labelsize=tick_fsize)
+    g.ax.set_xlabel(xlabel="Query", fontsize=xlabel_fsize)
+    g.ax.set_ylabel(ylabel="I/O Ratio", fontsize=ylabel_fsize)
+
+    for i in range(len(g.ax.patches)):
+        y_pos = g.ax.patches[i].get_height()
+        g.ax.annotate(f"{g.ax.patches[i].get_height():.1f}x", (g.ax.patches[i].get_x() - 0.01 + g.ax.patches[i].get_width() / 2., y_pos+0.1),
+            ha='center', va="bottom", fontsize=leg_size*0.8, color='k', annotation_clip=False)
+
+    # plt.axhline(y=1, color='k', linewidth='0.1')
+
+    plt.grid(which="major", axis="y")
+
+    g.savefig("IO_SPEEDUP.pdf")
 
 def main():
     if len(sys.argv) < 2:
@@ -418,12 +522,15 @@ def main():
         # graphs.append(("END_2_END", host_ndp_plot()))
         # graphs.append(("END_END_OVERHEAD", secndp_overheads()))
         # graphs.append(("HETERO_TEE", tee_overhead()))
-        # graphs.append(("REL_NDP", end_end_rel_ndp()))
-        graphs.append(("SEC_STORAGE", ssd_sec_storage_overheads()))
+        graphs.append(("REL_NDP", end_end_rel_ndp()))
+        # graphs.append(("SEC_STORAGE", ssd_sec_storage_overheads()))
 
     if sys.argv[1] == "sel":
-        selectivity_vs_query()
-        # size_vs_query()
+        # selectivity_vs_query()
+        size_vs_query()
+
+    if sys.argv[1] == "io-speed":
+        io_speedup()
 
     # for name, graph in graphs:
     #     filename = f"{name}.pdf"
