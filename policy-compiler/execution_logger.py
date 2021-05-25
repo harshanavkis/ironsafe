b00dc52519aa289e1601081300a1ac4d74b6af79
harshanavkis <harshanavkis@gmail.com>
2021-05-25 22:29:23 +0200

    Execution logging: log split query, user identity, time

---
+++ policy-compiler/execution_logger.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import datetime
+
+def log_query_execution(host_query, storage_query, user_key):
+    f = open(os.environ["LOG_FILE"], "a")
+
+    log_data = "{},{},{},{}".format(
+        datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"),
+        user_key,
+        host_query,
+        storage_query
+    )
+
+    f.write(log_data)
+    f.flush()
+    os.fsync(f.fileno())
+
+    import pdb; pdb.set_trace()
+
+    f.close()
+
+def main():
+    host_query    = sys.argv[1]
+    storage_query = sys.argv[2]
+    user_key      = sys.argv[3]
+
+    log_query_execution(host_query, storage_query, user_key)
+
+if __name__ == "__main__":
+    main()
