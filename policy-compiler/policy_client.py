fe173c8fad149e7ca0c2845ba2fccf257d6818d0
harshanavkis <harshanavkis@gmail.com>
2021-05-27 18:55:03 +0200

    Bind policy server to particular ip, include initial scone examples

--- policy-compiler/policy_client.py
+++ policy-compiler/policy_client.py
@@ -5,8 +5,12 @@ import time
 
 # SERVER_IP=127.0.0.1 SERVER_PORT=5000 python3 policy_client.py user_policy.txt
 
+# docker run --rm  $MOUNT_SGXDEVICE -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SERVER_IP=172.17.0.2 -e SERVER_PORT=5000 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 sconecuratedimages/apps:python-3.7-alpine python policy_client.py user_policy.txt
+
 def send_policy_to_server(user_policy):
     # print(len(user_policy))
+    # print(os.environ["SERVER_IP"])
+    # print(os.environ["SERVER_PORT"])
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((os.environ["SERVER_IP"], int(os.environ["SERVER_PORT"])))
         s.sendall(user_policy.encode())
--- policy-compiler/policy_server.py
+++ policy-compiler/policy_server.py
@@ -8,10 +8,12 @@ from policy_checker import check_policy, check_node_policy_compliance
 
 # SERVER_IP=127.0.0.1 IDENTITY_FILE=dummy-user.pub STORAGE_FW_VERS_DB=storage_version.csv SERVER_PORT=5000 python3 policy_server.py dummy_storage_attr.json
 
+# docker run --rm  $MOUNT_SGXDEVICE -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 -e SERVER_IP=172.17.0.2 -e IDENTITY_FILE=dummy-user.pub -e STORAGE_FW_VERS_DB=storage_version.csv -e SERVER_PORT=5000 sconecuratedimages/apps:python-3.7-alpine python policy_server.py dummy_storage_attr.json
+
 def run_server(storage_attr_json):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', int(os.environ["SERVER_PORT"])))
+        s.bind((os.environ["SERVER_IP"], int(os.environ["SERVER_PORT"])))
         s.listen()
 
         conn, addr = s.accept()
