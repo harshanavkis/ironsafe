e9349af68af443d31cacdc93e6133a0cac3bf9a3
harshanavkis <harshanavkis@gmail.com>
2021-05-27 19:02:17 +0200

    Bind logging server to particular ip, include initial scone examples

--- policy-compiler/logging_client.py
+++ policy-compiler/logging_client.py
@@ -5,6 +5,8 @@ import time
 
 # SERVER_IP=127.0.0.1 SERVER_PORT=5000 python3 logging_client.py
 
+# docker run --rm  $MOUNT_SGXDEVICE -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 -e SERVER_IP=172.17.0.2 -e SERVER_PORT=5000 sconecuratedimages/apps:python-3.7-alpine python logging_client.py
+
 def send_data_to_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data = "{\"host_query\": \"bitch\",\"storage_query\": \"bitch\",\"user_key\": \"bitch\"}"
--- policy-compiler/logging_server.py
+++ policy-compiler/logging_server.py
@@ -4,12 +4,14 @@ import ast
 
 # LOG_FILE=bitch-log SERVER_PORT=5000 python3 logging_server.py
 
+# docker run --rm  $MOUNT_SGXDEVICE -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 -e SERVER_IP=172.17.0.2 -e LOG_FILE=bitch-log -e SERVER_PORT=5000 sconecuratedimages/apps:python-3.7-alpine python logging_server.py
+
 from execution_logger import log_query_execution
 
 def run_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind(('', int(os.environ["SERVER_PORT"])))
+        s.bind((os.environ["SERVER_IP"], int(os.environ["SERVER_PORT"])))
         s.listen()
 
         conn, addr = s.accept()
