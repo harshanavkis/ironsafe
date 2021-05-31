1225b5e8c55d15d8b1e04993c888ed75e5b75c14
harshanavkis <harshanavkis@gmail.com>
2021-05-31 15:33:11 +0200

    Test only if volume doesn't already exist

--- policy-compiler/run_logging_exp.sh
+++ policy-compiler/run_logging_exp.sh
@@ -24,12 +24,12 @@ function test_encr_vol() {
     SCONE_FSPF_KEY=$(cat $ORIG_VOL/keytag | awk '{print $11}')
     SCONE_FSPF_TAG=$(cat $ORIG_VOL/keytag | awk '{print $9}')
 
-    docker run -it --rm  $MOUNT_SGXDEVICE -v "$PWD/volume:/data" -v /tmp/secndp/:/data-original -v "$PWD:/usr/src/myapp" -w "/usr/src/myapp" -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 sconecuratedimages/apps:python-3.7-alpine /bin/bash -c "SCONE_FSPF_KEY=$SCONE_FSPF_KEY SCONE_FSPF_TAG=$SCONE_FSPF_TAG SCONE_FSPF=/data/fspf.pb LOG_FILE=/data/secndp_log python test_scone_file_shield.py"
+    docker run --rm  $MOUNT_SGXDEVICE -v "$PWD/volume:/data" -v /tmp/secndp/:/data-original -v "$PWD:/usr/src/myapp" -w "/usr/src/myapp" -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 sconecuratedimages/apps:python-3.7-alpine /bin/bash -c "SCONE_FSPF_KEY=$SCONE_FSPF_KEY SCONE_FSPF_TAG=$SCONE_FSPF_TAG SCONE_FSPF=/data/fspf.pb LOG_FILE=/data/secndp_log python test_scone_file_shield.py"
 }
 
 if [ ! -f "$ENC_VOL/fspf.pb" ]; then
     echo "Setting up encrypted volume"
     setup_encr_vol
+    test_encr_vol
 fi
 
-test_encr_vol
