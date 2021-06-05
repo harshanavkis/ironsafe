9d5fc5cee66c1efdf80fbe79799eeff5969dc62c
harshanavkis <harshanavkis@gmail.com>
2021-06-05 15:14:08 +0000

    Revert host startup changes

--- benchmark/host_setup_benchmark.sh
+++ benchmark/host_setup_benchmark.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-for x in /sys/devices/system/cpu/*/cpufreq/; do echo 2000000 | sudo tee $x/scaling_max_freq; done
-
 sudo ip link set enp2s0f1 up
 sudo ip addr flush dev enp2s0f1
 sudo ip addr add 10.0.42.22/24 dev enp2s0f1
