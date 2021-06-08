8c1b9183249f94c1d016a168058cad36cc5193d9
harshanavkis <harshanavkis@gmail.com>
2021-06-08 20:38:00 +0000

    Extract scone binary from docker image
    docker run with scone breaks, no idea why.

--- build_docker_imgs.sh
+++ build_docker_imgs.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-docker build -f benchmark/scone-stuff/sec-ndp -t host-ndp .
-sleep 5
-docker build -f benchmark/scone-stuff/vanilla-ndp -t vanilla-ndp .
-sleep 5
-docker build -f benchmark/scone-stuff/pure-host -t pure-host .
-sleep 5
-docker build -f benchmark/scone-stuff/pure-host-sec -t pure-host-sec .
+mkdir -p $SEC_BUILD_DIR
+
+#docker build -f benchmark/scone-stuff/sec-ndp -t host-ndp .
+hs=$(docker create host-ndp)
+docker cp $hs:/sqlite-ndp/host/secure/host-ndp $SEC_BUILD_DIR
+docker rm $hs
+#sleep 5
+#docker build -f benchmark/scone-stuff/vanilla-ndp -t vanilla-ndp .
+#sleep 5
+#docker build -f benchmark/scone-stuff/pure-host -t pure-host .
+#sleep 5
+#docker build -f benchmark/scone-stuff/pure-host-sec -t pure-host-sec .
+#phs=$(docker create pure-host-sec)
+#docker cp $phs:/fresh-sqlite/hello-query $SEC_BUILD_DIR
+#docker rm $phs
