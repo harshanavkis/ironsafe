81729485ed2f6ef394ebfa4fff9eb96da53843aa
harshanavkis <harshanavkis@gmail.com>
2021-06-08 23:47:57 +0000

    Add docs to docker build images script.

--- build_docker_imgs.sh
+++ build_docker_imgs.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
+# SEC_BUID_DIR=$PROJECT_ROOT_DIR/sec-bin
 mkdir -p $SEC_BUILD_DIR
 
 #docker build -f benchmark/scone-stuff/sec-ndp -t host-ndp .
-hs=$(docker create host-ndp)
-docker cp $hs:/sqlite-ndp/host/secure/host-ndp $SEC_BUILD_DIR
-docker rm $hs
+#hs=$(docker create host-ndp)
+#docker cp $hs:/sqlite-ndp/host/secure/host-ndp $SEC_BUILD_DIR
+#docker rm $hs
 #sleep 5
 #docker build -f benchmark/scone-stuff/vanilla-ndp -t vanilla-ndp .
 #sleep 5
 #docker build -f benchmark/scone-stuff/pure-host -t pure-host .
 #sleep 5
 #docker build -f benchmark/scone-stuff/pure-host-sec -t pure-host-sec .
-#phs=$(docker create pure-host-sec)
-#docker cp $phs:/fresh-sqlite/hello-query $SEC_BUILD_DIR
-#docker rm $phs
+phs=$(docker create pure-host-sec)
+docker cp $phs:/fresh-sqlite/hello-query $SEC_BUILD_DIR
+docker rm $phs
