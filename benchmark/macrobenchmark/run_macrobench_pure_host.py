3492a763ee5f805f7b1aa6b403d8da9f62eb86cf
harshanavkis <harshanavkis@gmail.com>
2021-06-08 22:33:45 +0000

    Update scone benchmark script to run directly from binary.

--- benchmark/macrobenchmark/run_macrobench_pure_host.py
+++ benchmark/macrobenchmark/run_macrobench_pure_host.py
@@ -22,6 +22,7 @@ NOW = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 ROOT_DIR = os.path.realpath("../../")
 CURR_DIR = os.path.realpath(".")
+SEC_BIN_DIR = os.path.join(ROOT_DIR, "sec-bin")
 
 MERK_FILE = "merkle-tree-{}.bin"
 
@@ -101,9 +102,9 @@ def run_pure_host_ns(kind, stats):
     merk_file = os.path.join("/data", f"{MERK_FILE.format(scale_factor)}")
     exe = os.path.join(ROOT_DIR, "benchmark/macrobenchmark/selectivity-effect/run_pure_host_non_secure.sh")
 
-    cpu_df = pd.read_csv(sys.argv[1])
-    cpu_df = cpu_df.set_index("query").to_dict()
-    cpu_df = cpu_df["cpu"]
+    #cpu_df = pd.read_csv(sys.argv[1])
+    #cpu_df = cpu_df.set_index("query").to_dict()
+    #cpu_df = cpu_df["cpu"]
 
     for i in df:
         if i[0] in ignore_queries:
@@ -112,7 +113,7 @@ def run_pure_host_ns(kind, stats):
         #proc = subprocess.Popen(cmd)
         #proc.wait()
 
-        cpus = cpu_df[i[0]]
+        #cpus = cpu_df[i[0]]
 
         clear_cache()
         cmd = [
@@ -160,39 +161,39 @@ def run_pure_host_sec(kind, stats):
     #proc = subprocess.Popen(cmd)
     #proc.wait()
 
-    db  = os.path.join("/data", f"TPCH-{scale_factor}-fresh-enc.db")
-    merk_file = os.path.join("/data", f"{MERK_FILE.format(scale_factor)}")
+    db  = os.path.join(data_dir, f"TPCH-{scale_factor}-fresh-enc.db")
+    merk_file = os.path.join(data_dir, f"{MERK_FILE.format(scale_factor)}")
 
     #cmd = ["sudo", "systemctl", "restart", "docker"]
     #proc = subprocess.Popen(cmd)
     #proc.wait()
 
-    cpu_df = pd.read_csv(sys.argv[1])
-    cpu_df = cpu_df.set_index("query").to_dict()
-    cpu_df = cpu_df["cpu"]
+    #cpu_df = pd.read_csv(sys.argv[1])
+    #cpu_df = cpu_df.set_index("query").to_dict()
+    #cpu_df = cpu_df["cpu"]
+    
+    binary = os.path.join(SEC_BIN_DIR, "hello-query")
+    scone_env = os.environ.copy()
+    scone_env["SCONE_VERSION"] = "1"
+    scone_env["SCONE_HEAP"] = "4G"
 
     for i in df:
         if i[0] in ignore_queries:
             continue
         clear_cache()
 
-        cpus = cpu_df[i[0]]
+        #cpus = cpu_df[i[0]]
 
         cmd = [
-        "docker",
-        "run",
-        #"--cpuset-cpus=0",
-        "--mount",
-        f"type=bind,source={data_dir},target=/data",
-        #f"--cpus={CPUS}",
-        "pure-host-sec",
-        "/bin/bash",
-        "-c",
-        "SCONE_VERSION=1 SCONE_HEAP=4G ./hello-query {} {} kun \"{}\"".format(merk_file, db, i[1])
-    ]
+            binary,
+            merk_file,
+            db,
+            "kun",
+            "{}".format(i[1])
+            ]
         print(cmd)
 
-        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True)
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True, env=scone_env)
         proc.wait()
 
         process_output(proc, kind, i[0], stats)
@@ -202,8 +203,8 @@ def main():
     setup_exp()
 
     benchmarks = {
-        "pure-host-non-secure": run_pure_host_ns,
-        #"pure-host-secure": run_pure_host_sec,
+        #"pure-host-non-secure": run_pure_host_ns,
+        "pure-host-secure": run_pure_host_sec,
     }
 
     for name, benchmark in benchmarks.items():
@@ -212,7 +213,7 @@ def main():
     df = pd.DataFrame(stats)
     sf = os.environ["SCALE_FACTOR"]
     # cpus = os.environ["CPU_BENCH"]
-    df.to_csv(f"pure_host_macrobench-{sf}-cpuset-0-{NOW}.csv", index=False)
+    df.to_csv(f"pure_host_macrobench-{sf}-{NOW}.csv", index=False)
 
 if __name__=="__main__":
     main()
