cd362987d63856347b89b477700a90b3b077413e
Harshavardhan Unnibhavi <harshanavkis@gmail.com>
2021-06-24 01:21:00 +0200

    Non-secure use cases

--- policy-compiler/run_policy_exp.sh
+++ policy-compiler/run_policy_exp.sh
@@ -24,7 +24,7 @@ function setup_policy_server() {
 
     EXP_OP=$1
 
-    docker run --rm  $MOUNT_SGXDEVICE --network=host -v "$PWD/volume:/data" -v /tmp/secndp/:/data-original -v "$PWD:/usr/src/myapp" -w /usr/src/myapp -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 -e SERVER_IP=127.0.0.1 -e IDENTITY_FILE=dummy-user.pub -e STORAGE_FW_VERS_DB=storage_version.csv -e SERVER_PORT=9000 -e LOG_FILE=/data/secndp_log -e DATA_ACCESS_POLICY=/data/user_data_access_policy.json sconecuratedimages/apps:python-3.7-alpine /bin/bash -c "SCONE_FSPF_KEY=$SCONE_FSPF_KEY SCONE_FSPF_TAG=$SCONE_FSPF_TAG SCONE_FSPF=/data/fspf.pb python policy_server.py $EXP_OP dummy_storage_attr.json"
+    docker run --rm  $MOUNT_SGXDEVICE --network=host -v "$PWD/volume:/data" -v /tmp/secndp/:/data-original -v "$PWD:/usr/src/myapp" -w /usr/src/myapp -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 -e SERVER_IP=127.0.0.1 -e IDENTITY_FILE=dummy-user.pub -e STORAGE_FW_VERS_DB=storage_version.csv -e SERVER_PORT=9000 -e LOG_FILE=/data/secure-secndp_log -e DATA_ACCESS_POLICY=/data/user_data_access_policy.json sconecuratedimages/apps:python-3.7-alpine /bin/bash -c "SCONE_FSPF_KEY=$SCONE_FSPF_KEY SCONE_FSPF_TAG=$SCONE_FSPF_TAG SCONE_FSPF=/data/fspf.pb python policy_server.py $EXP_OP dummy_storage_attr.json"
 }
 
 function run_policy_client() {
@@ -59,6 +59,16 @@ function test_encr_vol() {
     docker run --rm  $MOUNT_SGXDEVICE -v "$PWD/volume:/data" -v /tmp/secndp/:/data-original -v "$PWD:/usr/src/myapp" -w "/usr/src/myapp" -e SCONE_HEAP=256M -e SCONE_MODE=HW -e SCONE_ALLOW_DLOPEN=2 -e SCONE_ALPINE=1 -e SCONE_VERSION=1 sconecuratedimages/apps:python-3.7-alpine /bin/bash -c "SCONE_FSPF_KEY=$SCONE_FSPF_KEY SCONE_FSPF_TAG=$SCONE_FSPF_TAG SCONE_FSPF=/data/fspf.pb LOG_FILE=/data/user_data_access_policy.json python test_scone_file_shield.py"
 }
 
+function non_secure_policy_server() {
+    EXP_OP=$1
+
+    STORAGE_FW_VERS_DB=storage_version.csv SERVER_PORT=9000 LOG_FILE=$ENC_VOL/nonsecure-secndp_log SERVER_IP=127.0.0.1 DATA_ACCESS_POLICY=user_data_access_policy.json python3 policy_server.py $EXP_OP dummy_storage_attr.json
+}
+
+function non_secure_policy_client() {
+    SERVER_IP=127.0.0.1 SERVER_PORT=9000 python3 policy_client.py user_policy.txt
+}
+
 determine_sgx_device
 
 if [ ! -f "$ENC_VOL/fspf.pb" ]; then
@@ -72,75 +82,152 @@ sleep 5
 
 DATE=$(date +"%Y-%m-%d-%H-%M")
 ITER=$1
+SEC=$2
 
-echo "Running use case 1: timely deletion"
-for i in $(eval echo {1..$ITER})
-do
-    clear_page_cache
-    setup_policy_server "1" &
+if [ "$SEC" = "secure" ]; then
+    echo "Running use case 1: timely deletion"
+    for i in $(eval echo {1..$ITER})
+    do
+        clear_page_cache
+        setup_policy_server "1" &
+        sleep 10
+        result=$(run_policy_client)
+        echo "$i, $result" >> secure-use-case-one-$DATE.csv
+        sleep 5
+    done
+
     sleep 10
-    result=$(run_policy_client)
-    echo "$i, $result" >> use-case-one-$DATE.csv
-    sleep 5
-done
 
-sleep 10
+    #exit 0
 
-#exit 0
+    echo "Running use case 2: indiscr use case"
+    for i in $(eval echo {1..$ITER})
+    do
+        clear_page_cache
+        setup_policy_server "2" &
+        sleep 10
+        result=$(run_policy_client)
+        echo "$i, $result" >> secure-use-case-two-$DATE.csv
+        sleep 5
+    done
 
-echo "Running use case 2: indiscr use case"
-for i in $(eval echo {1..$ITER})
-do
-    clear_page_cache
-    setup_policy_server "2" &
     sleep 10
-    result=$(run_policy_client)
-    echo "$i, $result" >> use-case-two-$DATE.csv
-    sleep 5
-done
 
-sleep 10
-
-echo "Running use case 3: obtain shared data"
-setup_policy_server "4" &
-sleep 10
-result=$(run_policy_client)
-for i in $(eval echo {1..$ITER})
-do
-    clear_page_cache
-    setup_policy_server "3" &
-    sleep 10
-    result=$(run_policy_client)
-    echo "$i, $result" >> use-case-three-$DATE.csv
-    sleep 5
-done
-
-sleep 10
-
-echo "Running use case 4: risk agnostic"
-for i in $(eval echo {1..$ITER})
-do
-    sudo rm $PWD/$ENC_VOL/$LOG_FILE
-    clear_page_cache
+    echo "Running use case 3: obtain shared data"
     setup_policy_server "4" &
     sleep 10
     result=$(run_policy_client)
-    echo "$i, $result" >> use-case-four-$DATE.csv
-    sleep 5
-done
+    for i in $(eval echo {1..$ITER})
+    do
+        clear_page_cache
+        setup_policy_server "3" &
+        sleep 10
+        result=$(run_policy_client)
+        echo "$i, $result" >> secure-use-case-three-$DATE.csv
+        sleep 5
+    done
 
-sleep 10
-
-echo "Running use case 5: hiding breaches"
-for i in $(eval echo {1..$ITER})
-do
-    sudo rm $PWD/$ENC_VOL/$LOG_FILE
-    clear_page_cache
-    setup_policy_server "5" &
     sleep 10
-    result=$(run_policy_client)
-    echo "$i, $result" >> use-case-five-$DATE.csv
-    sleep 5
-done
 
-sleep 10
+    echo "Running use case 4: risk agnostic"
+    for i in $(eval echo {1..$ITER})
+    do
+        sudo rm $PWD/$ENC_VOL/$LOG_FILE
+        clear_page_cache
+        setup_policy_server "4" &
+        sleep 10
+        result=$(run_policy_client)
+        echo "$i, $result" >> secure-use-case-four-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+
+    echo "Running use case 5: hiding breaches"
+    for i in $(eval echo {1..$ITER})
+    do
+        sudo rm $PWD/$ENC_VOL/$LOG_FILE
+        clear_page_cache
+        setup_policy_server "5" &
+        sleep 10
+        result=$(run_policy_client)
+        echo "$i, $result" >> secure-use-case-five-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+fi
+
+if [ "$SEC" = "non-secure" ]; then
+    echo "Running use case 1: timely deletion"
+    for i in $(eval echo {1..$ITER})
+    do
+        clear_page_cache
+        non_secure_policy_server "1" &
+        sleep 10
+        result=$(non_secure_policy_client)
+        echo "$i, $result" >> non-secure-use-case-one-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+
+    #exit 0
+
+    echo "Running use case 2: indiscr use case"
+    for i in $(eval echo {1..$ITER})
+    do
+        clear_page_cache
+        non_secure_policy_server "2" &
+        sleep 10
+        result=$(non_secure_policy_client)
+        echo "$i, $result" >> non-secure-use-case-two-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+
+    echo "Running use case 3: obtain shared data"
+    non_secure_policy_server "4" &
+    sleep 10
+    result=$(non_secure_policy_client)
+    for i in $(eval echo {1..$ITER})
+    do
+        clear_page_cache
+        non_secure_policy_server "3" &
+        sleep 10
+        result=$(non_secure_policy_client)
+        echo "$i, $result" >> non-secure-use-case-three-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+
+    echo "Running use case 4: risk agnostic"
+    for i in $(eval echo {1..$ITER})
+    do
+        sudo rm $PWD/$ENC_VOL/nonsecure-$LOG_FILE
+        clear_page_cache
+        non_secure_policy_server "4" &
+        sleep 10
+        result=$(non_secure_policy_client)
+        echo "$i, $result" >> non-secure-use-case-four-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+
+    echo "Running use case 5: hiding breaches"
+    for i in $(eval echo {1..$ITER})
+    do
+        sudo rm $PWD/$ENC_VOL/nonsecure-$LOG_FILE
+        clear_page_cache
+        non_secure_policy_server "5" &
+        sleep 10
+        result=$(non_secure_policy_client)
+        echo "$i, $result" >> non-secure-use-case-five-$DATE.csv
+        sleep 5
+    done
+
+    sleep 10
+fi
